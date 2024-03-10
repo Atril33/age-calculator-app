f194cc88966ed2ddad60725bf732d5bf2debf776
syed atril <atril3123@gmail.com>
2024-03-10 21:09:39 +0500

    Add Years Functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,61 @@
-var inputFields = document.getElementsByClassName('inputField');
+var inputFields = document.querySelectorAll('.inputField');
 var dataOutput = document.getElementsByClassName('dataOutput');
 var resultButton = document.getElementsByClassName('button');
 var currentDate = Date();
 var dateToArray = currentDate.split(' ');
 var finalDate = dateToArray.splice(1, 3);
-var ClickButton = function () {
-    console.log(resultButton);
+var years;
+var months;
+var days;
+finalDate[0] = finalDate[0].toLowerCase();
+finalDate[1] = Number(finalDate[1]);
+if (finalDate[0] == 'jan')
+    finalDate[0] = 1;
+if (finalDate[0] == 'feb')
+    finalDate[0] = 2;
+if (finalDate[0] == 'mar')
+    finalDate[0] = 3;
+if (finalDate[0] == 'apr')
+    finalDate[0] = 4;
+if (finalDate[0] == 'may')
+    finalDate[0] = 5;
+if (finalDate[0] == 'jun')
+    finalDate[0] = 6;
+if (finalDate[0] == 'jul')
+    finalDate[0] = 7;
+if (finalDate[0] == 'aug')
+    finalDate[0] = 8;
+if (finalDate[0] == 'sep')
+    finalDate[0] = 9;
+if (finalDate[0] == 'oct')
+    finalDate[0] = 10;
+if (finalDate[0] == 'nov')
+    finalDate[0] = 11;
+if (finalDate[0] == 'dec')
+    finalDate[0] = 12;
+var yearsDisplay;
+var monthsDisplay;
+var daysDisplay;
+var ageCalculation = function () {
+    days = inputFields[0].value;
+    months = inputFields[1].value;
+    years = inputFields[2].value;
+    if (finalDate[0] < months) {
+        yearsDisplay = (finalDate[2] - years) - 1;
+        console.log(yearsDisplay);
+    }
+    if (finalDate[0] > months) {
+        yearsDisplay = finalDate[2] - years;
+    }
+    if (finalDate[0] == months && finalDate[1] >= days) {
+        yearsDisplay = finalDate[2] - years;
+    }
+    if (finalDate[1] <= days) {
+        yearsDisplay = (finalDate[2] - years) - 1;
+        console.log(yearsDisplay);
+    }
 };
-resultButton[0].addEventListener('click', ClickButton);
+var clickButton = function () {
+    ageCalculation();
+};
+resultButton[0].addEventListener('click', clickButton);
--- js/main.ts
+++ js/main.ts
@@ -1,13 +1,66 @@
-let inputFields: HTMLCollectionOf<Element> = document.getElementsByClassName('inputField');
-let dataOutput: HTMLCollectionOf<Element> = document.getElementsByClassName('dataOutput');
-let resultButton: HTMLCollectionOf<Element> = document.getElementsByClassName('button');
-let currentDate: any = Date();
-let dateToArray: any = currentDate.split(' ');
-let finalDate: Array<string> = dateToArray.splice(1,3)
+const inputFields: NodeListOf<Element> = document.querySelectorAll('.inputField');
+const dataOutput: HTMLCollectionOf<Element> = document.getElementsByClassName('dataOutput');
+const resultButton: HTMLCollectionOf<Element> = document.getElementsByClassName('button');
+const currentDate: any = Date();
+const dateToArray: any = currentDate.split(' ');
+const finalDate: any = dateToArray.splice(1,3)
+let years: any;
+let months: any;
+let days: any;
+
+finalDate[0] = finalDate[0].toLowerCase()
+finalDate[1] = Number(finalDate[1])
+
+if(finalDate[0] == 'jan') finalDate[0] = 1;
+if(finalDate[0] == 'feb') finalDate[0] = 2;
+if(finalDate[0] == 'mar') finalDate[0] = 3;
+if(finalDate[0] == 'apr') finalDate[0] = 4
+if(finalDate[0] == 'may') finalDate[0] = 5
+if(finalDate[0] == 'jun') finalDate[0] = 6
+if(finalDate[0] == 'jul') finalDate[0] = 7
+if(finalDate[0] == 'aug') finalDate[0] = 8
+if(finalDate[0] == 'sep') finalDate[0] = 9
+if(finalDate[0] == 'oct') finalDate[0] = 10
+if(finalDate[0] == 'nov') finalDate[0] = 11
+if(finalDate[0] == 'dec') finalDate[0] = 12
+
+let yearsDisplay: any;
+let monthsDisplay: any;
+let daysDisplay: any;
+
+const ageCalculation = () => {
+  days = inputFields[0].value
+  months = inputFields[1].value
+  years = inputFields[2].value
 
 
-const ClickButton = (): void => {
-  console.log(resultButton)
+  if(finalDate[0] < months) {
+   
+    yearsDisplay = (finalDate[2] - years) - 1
+    console.log(yearsDisplay)
+
+  } 
+
+   if(finalDate[0] > months) {
+    yearsDisplay = finalDate[2] - years
+  } 
+  
+   if (finalDate[0] == months && finalDate[1] >= days) {
+    yearsDisplay = finalDate[2] - years
+  }
+
+  if (finalDate[1] <= days) {
+    yearsDisplay = (finalDate[2] - years) - 1
+    console.log(yearsDisplay)
+  }
+ 
+
+
+
+ }
+
+const clickButton = (): void => {
+  ageCalculation()
 }
 
-resultButton[0].addEventListener('click', ClickButton)
+ resultButton[0].addEventListener('click', clickButton) 
