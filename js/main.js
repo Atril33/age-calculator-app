fdf618b0d7fedbeaa6f70154090ad2dd0268bf00
syed atril <atril3123@gmail.com>
2024-03-10 13:55:53 +0500

    Add Current Date Function In App

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 var inputFields = document.getElementsByClassName('inputField');
 var dataOutput = document.getElementsByClassName('dataOutput');
 var resultButton = document.getElementsByClassName('button');
+var currentDate = Date();
+var dateToArray = currentDate.split(' ');
+console.log(dateToArray.splice(1, 3));
+var ClickButton = function () {
+    console.log(resultButton);
+};
+resultButton[0].addEventListener('click', ClickButton);
--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,12 @@
 let inputFields: HTMLCollectionOf<Element> = document.getElementsByClassName('inputField');
 let dataOutput: HTMLCollectionOf<Element> = document.getElementsByClassName('dataOutput');
 let resultButton: HTMLCollectionOf<Element> = document.getElementsByClassName('button');
+let currentDate: any = Date();
+let dateToArray = currentDate.split(' ');
+console.log(dateToArray.splice(1,3))
 
+const ClickButton = (): void => {
+  console.log(resultButton)
+}
+
+resultButton[0].addEventListener('click', ClickButton)
